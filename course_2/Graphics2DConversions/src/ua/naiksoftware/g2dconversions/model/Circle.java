0e55782bcf7e1ec953d41d2733225eaf712d5b14
Naik <naiksav4enko@gmail.com>
2015-10-10 12:08:21 +0300

    Refactor names

--- course_2/Graphics2DConversions/src/ua/naiksoftware/g2dconversions/model/Circle.java
+++ course_2/Graphics2DConversions/src/ua/naiksoftware/g2dconversions/model/Circle.java
@@ -3,8 +3,6 @@ package ua.naiksoftware.g2dconversions.model;
 import com.sun.javafx.geom.Matrix3f;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
 import ua.naiksoftware.g2dconversions.Operation;
 
 /**
@@ -12,62 +10,59 @@ import ua.naiksoftware.g2dconversions.Operation;
  */
 public class Circle extends Shape {
 
-    private Point2D cernLeft, cernTop, cernRight, cernDown;
-    private Point2D curve1, curve2, curve3, curve4;
+    private Point2D midLeft, midTop, midRight, midDown;
+    private Point2D leftTop, rightTop, rightDown, leftDown;
 
     public Circle(float cx, float cy, float radius) {
-        cernLeft = new Point2D(cx - radius, cy);
-        cernTop = new Point2D(cx, cy - radius);
-        cernRight = new Point2D(cx + radius, cy);
-        cernDown = new Point2D(cx, cy + radius);
-        curve1 = new Point2D(cx - radius, cy - radius);
-        curve2 = new Point2D(cx + radius, cy - radius);
-        curve3 = new Point2D(cx + radius, cy + radius);
-        curve4 = new Point2D(cx - radius, cy + radius);
+
+        midLeft = new Point2D(cx - radius, cy);
+        midTop = new Point2D(cx, cy - radius);
+        midRight = new Point2D(cx + radius, cy);
+        midDown = new Point2D(cx, cy + radius);
+        leftTop = new Point2D(cx - radius, cy - radius);
+        rightTop = new Point2D(cx + radius, cy - radius);
+        rightDown = new Point2D(cx + radius, cy + radius);
+        leftDown = new Point2D(cx - radius, cy + radius);
     }
 
     @Override
     public void transform(Matrix3f matrix) {
-        cernLeft = Operation.multiple(matrix, cernLeft);
-        cernTop = Operation.multiple(matrix, cernTop);
-        cernRight = Operation.multiple(matrix, cernRight);
-        cernDown = Operation.multiple(matrix, cernDown);
+        midLeft = Operation.multiple(matrix, midLeft);
+        midTop = Operation.multiple(matrix, midTop);
+        midRight = Operation.multiple(matrix, midRight);
+        midDown = Operation.multiple(matrix, midDown);
 
-        curve1 = Operation.multiple(matrix, curve1);
-        curve2 = Operation.multiple(matrix, curve2);
-        curve3 = Operation.multiple(matrix, curve3);
-        curve4 = Operation.multiple(matrix, curve4);
+        leftTop = Operation.multiple(matrix, leftTop);
+        rightTop = Operation.multiple(matrix, rightTop);
+        rightDown = Operation.multiple(matrix, rightDown);
+        leftDown = Operation.multiple(matrix, leftDown);
     }
 
     @Override
     public void draw(GraphicsContext graphicsContext) {
         graphicsContext.beginPath();
-        double deg = curve1.angle(cernLeft, cernTop);
+        graphicsContext.moveTo(midLeft.getX(), midLeft.getY());
 
-        Arc arc = new Arc();
-        arc.setCenterX(50.0f);
-        arc.setCenterY(50.0f);
-        arc.setRadiusX(25.0f);
-        arc.setRadiusY(25.0f);
-        arc.setStartAngle(45.0f);
-        arc.setLength(270.0f);
-        arc.setType(ArcType.OPEN);
+        graphicsContext.quadraticCurveTo(midLeft.midpoint(leftTop).getX(), midLeft.midpoint(leftTop).getY(),
+                midTop.getX(), midTop.getY());
 
-        //System.out.println("Deg: " + deg);
-        double angle = curve4.angle(curve1, curve4.add(0, -1));
-        System.out.println(angle);
-        graphicsContext.strokeArc(curve1.getX(), curve1.getY(), curve1.distance(curve2), curve1.distance(curve4), 90 - angle, 90, ArcType.ROUND);
-        /*graphicsContext.quadraticCurveTo(curve1.getX(), curve1.getY(), cernTop.getX(), cernTop.getY());
-        graphicsContext.quadraticCurveTo(curve2.getX(), curve2.getY(), cernRight.getX(), cernRight.getY());
-        graphicsContext.quadraticCurveTo(curve3.getX(), curve3.getY(), cernDown.getX(), cernDown.getY());
-        graphicsContext.quadraticCurveTo(curve4.getX(), curve4.getY(), cernLeft.getX(), cernLeft.getY());*/
-        //graphicsContext.closePath();
-        //graphicsContext.stroke();
+        graphicsContext.moveTo(midLeft.getX(), midLeft.getY());
+        graphicsContext.bezierCurveTo(midLeft.midpoint(leftTop).getX(), midLeft.midpoint(leftTop).getY(),
+                leftTop.midpoint(midTop).getX(), leftTop.midpoint(midTop).getY(),
+                midTop.getX(), midTop.getY());
 
-        graphicsContext.strokeLine(curve1.getX(), curve1.getY(), curve2.getX(), curve2.getY());
-        graphicsContext.strokeLine(curve1.getX(), curve1.getY(), curve4.getX(), curve4.getY());
+        graphicsContext.bezierCurveTo(midTop.midpoint(rightTop).getX(), midTop.midpoint(rightTop).getY(),
+                rightTop.midpoint(midRight).getX(), rightTop.midpoint(midRight).getY(),
+                midRight.getX(), midRight.getY());
 
-        graphicsContext.strokeLine(cernLeft.getX(), cernLeft.getY(), cernRight.getX(), cernRight.getY());
-        graphicsContext.strokeLine(cernTop.getX(), cernTop.getY(), cernDown.getX(), cernDown.getY());
+        graphicsContext.bezierCurveTo(midRight.midpoint(rightDown).getX(), midRight.midpoint(rightDown).getY(),
+                rightDown.midpoint(midDown).getX(), rightDown.midpoint(midDown).getY(),
+                midDown.getX(), midDown.getY());
+
+        graphicsContext.bezierCurveTo(midDown.midpoint(leftDown).getX(), midDown.midpoint(leftDown).getY(),
+                leftDown.midpoint(midLeft).getX(), leftDown.midpoint(midLeft).getY(),
+                midLeft.getX(), midLeft.getY());
+
+        graphicsContext.stroke();
     }
 }
